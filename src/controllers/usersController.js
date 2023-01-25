ae8939532d3ef0b60b6916879823669fd334616f
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-01-24 23:59:01 -0300

    build: bcrypt

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,4 +1,20 @@
+import { usersCollection } from "../database.js";
+
 export async function postUserController(req, res){
 
-    
+    const {name, email, password} = req.body;
+
+    const newUser = {
+        name,
+        email,
+        password
+    }
+
+    try{
+        await usersCollection.insertOne()
+
+    }catch(err){
+        res.status(500).send(err);
+
+    }
 }
